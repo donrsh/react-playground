9bb6e6d157cdfe220ab6248c3573e961b23f313f
Don Hsieh <don.shieh@gmail.com>
2018-10-17 15:17:12 +0800

    🔨 Refactor withTogglers

--- src/HOCs/withTogglers.js
+++ src/HOCs/withTogglers.js
@@ -1,91 +1,114 @@
-import { withState, withHandlers, withProps, compose, mapProps } from "recompose"
+import * as React from 'react'
+import { withProps, compose, mapProps } from "recompose"
 import * as R from "ramda"
 import shortid from "shortid"
 
-export const withTogglers = (...togglerConfigs) => {
-  const HOCs = R.pipe(
-    R.map(togglerConfig => {
-      let toggleName =
-        typeof togglerConfig === "string" ? togglerConfig : togglerConfig.name
+export const withTogglers = (...togglerConfigsOrThunk) => {
+  const tmpTogglerConfigsPropName = shortid.generate()
 
-      let defaultOpen = true
-      if (typeof togglerConfig === "object") {
-        defaultOpen = togglerConfig.defaultOpen
+  const HOCs = [
+    withProps(props => ({
+      [tmpTogglerConfigsPropName]: 
+        typeof togglerConfigsOrThunk[0] === 'function' ? 
+        togglerConfigsOrThunk[0](props) : 
+        togglerConfigsOrThunk
+    })),
+
+    (component) => class extends React.Component {
+      get togglerConfigs () {
+        return this.props[tmpTogglerConfigsPropName]
       }
 
-      let defaultSubState = undefined
-      if (typeof togglerConfig === "object") {
-        defaultSubState = togglerConfig.defaultSubState
+      constructor (...args) {
+        super(...args)
+        let togglerStates = {}
+
+        R.applyTo(this.togglerConfigs)(
+          R.forEach(togglerConfig => {
+            let toggleName = typeof togglerConfig === "string" ?
+              togglerConfig :
+              togglerConfig.name
+
+            if (togglerStates[toggleName]) {
+              throw new Error(`It seems there are duplicated \`toggleName\`: ${toggleName}. Check the configs of withTogglers.`)
+            }
+            togglerStates[toggleName] = {}
+
+            let defaultOpen = true
+            if (typeof togglerConfig === "object") {
+              defaultOpen = togglerConfig.defaultOpen
+            }
+            togglerStates[toggleName].open = defaultOpen
+
+            let defaultSubState = undefined
+            if (typeof togglerConfig === "object") {
+              defaultSubState = togglerConfig.defaultSubState
+            }
+            togglerStates[toggleName].subState = defaultSubState
+          })
+        )
+
+        this.state = { togglerStates }
       }
 
-      const tmpPropNames = R.times(shortid.generate, 5)
-      const [
-        tmpTogglerPropName,
-        tmpTogglerSetterPropName,
-        tmpOpenTogglerPropName,
-        tmpCloseTogglerPropName,
-        tmpToggleTogglerPropName
-      ] = tmpPropNames
+      getTogglerProps = (togglerName) => ({
+        isOpen: R.path([togglerName, 'open'], this.state.togglerStates),
+        subState: R.path([togglerName, 'subState'], this.state.togglerStates),
+        
+        open: (subState) => this.setState(R.evolve({
+          togglerStates: { [togglerName]: {
+            open: R.T,
+            subState: R.always(subState)
+          }}
+        })),
 
-      const togglePropName = `${toggleName}Toggler`
-
-      return [
-        withState(
-          tmpTogglerPropName,
-          tmpTogglerSetterPropName,
-          { open: defaultOpen, subState: defaultSubState }
-        ),
-
-        withHandlers({
-          [tmpOpenTogglerPropName]: ({
-            [tmpTogglerSetterPropName]: setToggler
-          }) => (subState) => {
-            setToggler({
-              open: true,
-              subState
-            })
-          },
-
-          [tmpCloseTogglerPropName]: ({
-            [tmpTogglerSetterPropName]: setToggler
-          }) => (subState) => {
-            setToggler({
-              open: false,
-              subState
-            })
-          },
-
-          [tmpToggleTogglerPropName]: ({
-            [tmpTogglerSetterPropName]: setToggler
-          }) => (subState) => {
-            setToggler(R.evolve({
+        close: (subState) => this.setState(R.evolve({
+          togglerStates: {
+            [togglerName]: {
+              open: R.F,
+              subState: R.always(subState)
+            }
+          }
+        })),
+        
+        toggle: (subState) => this.setState(R.evolve({
+          togglerStates: {
+            [togglerName]: {
               open: R.not,
               subState: R.always(subState)
-            }))
+            }
           }
-        }),
+        })),
 
-        withProps(props => {
-          return R.pipe(
-            R.assoc(
-              togglePropName,
-              {
-                isOpen: props[tmpTogglerPropName].open,
-                subState: props[tmpTogglerPropName].subState,
-                open: props[tmpOpenTogglerPropName],
-                close: props[tmpCloseTogglerPropName],
-                toggle: props[tmpToggleTogglerPropName],
-                set: props[tmpTogglerSetterPropName]
-              }
-            )
-          )(props)
-        }),
+        setSubState: (subState) => this.setState(R.evolve({
+          togglerStates: {
+            [togglerName]: {
+              subState: R.always(subState)
+            }
+          }
+        })),
+      })
 
-        mapProps(R.omit(tmpPropNames))
-      ]
-    }),
-    R.flatten
-  )(togglerConfigs)
+      get togglersProps() {
+        return R.pipe(
+          Object.keys,
+          R.map(togglerName => ([
+            `${togglerName}Toggler`,
+            this.getTogglerProps(togglerName)
+          ])),
+          R.fromPairs
+        )(this.state.togglerStates)
+      }
+
+      render () {
+        return React.createElement(component, 
+          { ...this.props, ...this.togglersProps }
+        )
+      }
+    },
+    
+    mapProps(R.omit([tmpTogglerConfigsPropName]))
+  ]
 
   return compose(...HOCs)
 }
